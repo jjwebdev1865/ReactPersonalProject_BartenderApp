43170a1463a35dcb6e46a1a58f54fb0212bc2311
James Jiracek <james.jiracek@slalom.com>
2022-03-10 08:48:49 -0500

    table component refactor

--- src/components/BeerForm.js
+++ src/components/BeerForm.js
@@ -2,31 +2,19 @@ import React from 'react';
 import './static/beerform.css';
 
 export default class BeerForm extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log("initiated");
-    };
-
-    changeHandlerBeerForm = (event) => {
-        let name = event.target.name;
-        let value = event.target.value;
-        this.setState({
-            [name]: value,
-        })
-    };
 
     render() {
         return (
             <form onSubmit={this.props.submitHandler}>
                 <h2>New Beer Form</h2>
                 <label htmlFor='beerName'>Beer Name:</label>
-                <input id='beerName' name='beerName' type="text" onChange={this.changeHandlerBeerForm}/>
+                <input id='beerName' name='beerName' type="text" onChange={this.props.handleChange}/>
 
                 <label htmlFor='beerPrice'>Price</label>
-                <input id='beerPrice' name='beerPrice' type="text" onChange={this.changeHandlerBeerForm} />
+                <input id='beerPrice' name='beerPrice' type="text" onChange={this.props.handleChange} />
 
                 <label htmlFor='beerRating'>Rating</label>
-                <select id='beerRating' name='beerRating' onChange={this.changeHandlerBeerForm}>
+                <select id='beerRating' name='beerRating' onChange={this.props.handleChange}>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
